3377b42f5ed25d6d748e0d15c13dcbf558cd86d7
Sebastian Tremblay <tremblay.se@northeastern.edu>
2025-04-09 15:07:34 -0400

    adding spinner for solver game board

--- frontend/src/pages/PlayAgainstSolverPage.tsx
+++ frontend/src/pages/PlayAgainstSolverPage.tsx
@@ -28,6 +28,7 @@ const GameContainer = styled.div`
 
 const GameSection = styled.div`
     text-align: center;
+    position: relative;
 `;
 
 const GameTitle = styled.h2`
@@ -82,6 +83,33 @@ const NewGameButton = styled.button`
     cursor: pointer;
     &:hover {
         background-color: #3a6b37;
+    }
+`;
+
+const LoadingOverlay = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(255, 255, 255, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 10;
+`;
+
+const Spinner = styled.div`
+    width: 40px;
+    height: 40px;
+    border: 4px solid #f3f3f3;
+    border-top: 4px solid #538d4e;
+    border-radius: 50%;
+    animation: spin 1s linear infinite;
+    
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
     }
 `;
 
@@ -200,6 +228,11 @@ const PlayAgainstSolverPage: React.FC = () => {
                     <GameSection>
                         <GameTitle>Solver's Game</GameTitle>
                         <GameBoard state={solverGameState} hideLetters={!isGameOver} />
+                        {isLoading && (
+                            <LoadingOverlay>
+                                <Spinner />
+                            </LoadingOverlay>
+                        )}
                     </GameSection>
                 </GameContainer>
                 <GuessInput
